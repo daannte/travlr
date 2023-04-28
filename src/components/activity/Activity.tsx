e17b3fd22b07e385ef62527608160b5e68fa1853
Dante <dkirsman@gmail.com>
2023-04-27 20:54:11 -0600

    Design change and added all features back except activity deletion

--- src/components/activity/Activity.tsx
+++ src/components/activity/Activity.tsx
@@ -1,11 +1,101 @@
-import "./Activity.css"
+import { useState } from "react";
+import deleteIcon from "../../assets/delete.svg";
+import editIcon from "../../assets/edit.svg";
+import confirmIcon from "../../assets/confirm.svg";
+import "./Activity.css";
 
-function Activity() {
-  return (
-    <div className="activity">
-      <h1>Activity</h1>
-    </div>
-  )
+interface ActivityList {
+    [key: number]: string[];
+}
+
+interface ActivityProps {
+    day: number;
+    activityId: string;
+    index: number;
+    setActivityList: React.Dispatch<React.SetStateAction<ActivityList>>;
+}
+
+function Activity({ day, activityId, index, setActivityList }: ActivityProps) {
+    const [isEditing, setIsEditing] = useState<boolean>(true);
+    const [startTime, setStartTime] = useState<string>("Start");
+    const [endTime, setEndTime] = useState<string>("End");
+    const [name, setName] = useState<string>("");
+
+    function handleEditValues(event: React.ChangeEvent<HTMLInputElement>) {
+        if (event.target.className === "time-start-edit") {
+            setStartTime(event.target.value);
+        } else if (event.target.className === "time-end-edit") {
+            setEndTime(event.target.value);
+        } else {
+            setName(event.target.value);
+        }
+    }
+
+    function handleActivityDelete() {
+        setActivityList((prevActivityList) => {
+            const newActivityList = { ...prevActivityList };
+            const activities = newActivityList[day];
+            activities.splice(index, 1);
+
+            for (let i = 1; i < activities.length; i++) {
+                activities[i] = "activity" + (i + 1);
+            }
+
+            return newActivityList;
+        });
+    }
+
+    return (
+        <div className="activity">
+            <div className="activity-info">
+                {isEditing ? (
+                    <>
+                        <div>
+                            <input
+                                className="time-start-edit"
+                                placeholder={startTime}
+                                onChange={handleEditValues}
+                            />{" "}
+                            <span> - </span>
+                            <input
+                                className="time-end-edit"
+                                placeholder={endTime}
+                                onChange={handleEditValues}
+                            />
+                        </div>
+                        <input
+                            className="name-edit"
+                            placeholder={name}
+                            onChange={handleEditValues}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <p className="activity-time">
+                            <span>{startTime} </span>-<span> {endTime}</span>
+                        </p>
+                        <p className="activity-name">{name}</p>
+                    </>
+                )}
+            </div>
+            <div className="activity-button">
+                {isEditing ? null : (
+                    <button className="delete-button" onClick={handleActivityDelete}>
+                        <img src={deleteIcon} alt="Delete Activity" />
+                    </button>
+                )}
+                {isEditing ? (
+                    <button className="confirm-button" onClick={() => setIsEditing(!isEditing)}>
+                        <img src={confirmIcon} alt="Confirm Edit" />
+                    </button>
+                ) : (
+                    <button className="edit-button" onClick={() => setIsEditing(!isEditing)}>
+                        <img src={editIcon} alt="Edit Activity" />
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default Activity;
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import Activity from "../activity/Activity";
+import deleteIcon from "../../assets/delete.svg";
+import addActivityIcon from "../../assets/addActivity.svg";
 import "./Card.css";
 
 interface ActivityList {
     [key: number]: string[];
 }
-
 interface CardProps {
     day: number;
     setActivityList: React.Dispatch<React.SetStateAction<ActivityList>>;
@@ -12,9 +13,57 @@ interface CardProps {
 }
 
 function Card({ day, setActivityList, activityList }: CardProps) {
+    function handleCardDelete() {
+        setActivityList((prevActivityList: ActivityList) => {
+            delete prevActivityList[day];
+            const fixedActivityList: ActivityList = {};
+            // Reassign the keys so the numbers are back in order
+            let i = 1;
+            for (const key in prevActivityList) {
+                fixedActivityList[i] = prevActivityList[key];
+                i++;
+            }
+            return fixedActivityList;
+        });
+    }
+    console.log(activityList);
+    function handleAddActivity() {
+        setActivityList((prevActivityList: ActivityList) => {
+            const newActivityList = { ...prevActivityList };
+            newActivityList[day] = [
+                ...newActivityList[day],
+                "activity" + (newActivityList[day].length + 1),
+            ];
+            return newActivityList;
+        });
+    }
+
+    function renderActivites() {
+        return activityList[day].map((activityId: string, index: number) => {
+            return (
+                <Activity
+                    key={index}
+                    day={day}
+                    activityId={activityId}
+                    index={index}
+                    setActivityList={setActivityList}
+                />
+            );
+        });
+    }
+
     return (
         <div className="card">
-            <h1>Card</h1>
+            <div className="card-header">
+                <h1 className="card-title">Day {day}</h1>
+                <button className="delete-card-icon" onClick={handleCardDelete}>
+                    <img src={deleteIcon} alt="Delete Card" />
+                </button>
+            </div>
+            {renderActivites()}
+            <button className="add-activity-icon" onClick={handleAddActivity}>
+                <img src={addActivityIcon} alt="Add Activity" />
+            </button>
         </div>
     );
 }
