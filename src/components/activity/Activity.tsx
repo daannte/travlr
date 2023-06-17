806726de8247e4772869b179eaf41825352b8bdd
Dante <dkirsman@gmail.com>
2023-06-17 11:51:31 -0600

    refactor: Made the drag handle an icon for better usage

--- src/components/activity/Activity.tsx
+++ src/components/activity/Activity.tsx
@@ -2,10 +2,9 @@ import { useContext, useEffect, useState } from "react";
 import { PlannerContext } from "../../App";
 import { Draggable } from "@hello-pangea/dnd";
 import TimeSelect from "../timeSelect/TimeSelect";
-
-import deleteIcon from "../../assets/deleteActivity.svg";
-import clockIcon from "../../assets/clock.svg";
 import "./Activity.css";
+
+import clockIcon from "../../assets/clock.svg";
 
 interface IFormInputs {
   startTime: Date | null;
@@ -122,31 +121,65 @@ function Activity({ day, activityIndex }: ActivityProps) {
         <div
           className="activity-container"
           {...provided.draggableProps}
-          {...provided.dragHandleProps}
           ref={provided.innerRef}
         >
-          <img
-            className="delete-icon"
-            src={deleteIcon}
-            alt="Delete Icon"
-            onClick={handleActivityDelete}
-          />
-          <h2 className="activity-destination">{activity.name}</h2>
-          <div
-            className="activity-time"
-            onClick={() => setIsChoosingTime(true)}
-          >
-            {renderTime()}
+          <div {...provided.dragHandleProps}>
+            <svg
+              width="22"
+              height="22"
+              fill="none"
+              stroke="gray"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              className="drag-handle-icon"
+            >
+              <path d="M9 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path>
+              <path d="M9 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path>
+              <path d="M9 20a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path>
+              <path d="M15 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path>
+              <path d="M15 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path>
+              <path d="M15 20a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"></path>
+            </svg>
           </div>
-          {isChoosingTime && (
-            <TimeSelect
-              startTime={startTime}
-              setStartTime={setStartTime}
-              endTime={endTime}
-              setEndTime={setEndTime}
-              onSubmit={onSubmit}
-            />
-          )}
+          <div className="activity-info-container">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="gray"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="delete-icon"
+              onClick={handleActivityDelete}
+            >
+              <polyline points="3 6 5 6 21 6"></polyline>
+              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+              <line x1="10" y1="11" x2="10" y2="17"></line>
+              <line x1="14" y1="11" x2="14" y2="17"></line>
+            </svg>
+            <h2 className="activity-destination">{activity.name}</h2>
+            <div
+              className="activity-time"
+              onClick={() => setIsChoosingTime(true)}
+            >
+              {renderTime()}
+            </div>
+            {isChoosingTime && (
+              <TimeSelect
+                startTime={startTime}
+                setStartTime={setStartTime}
+                endTime={endTime}
+                setEndTime={setEndTime}
+                onSubmit={onSubmit}
+              />
+            )}
+          </div>
         </div>
       )}
     </Draggable>
