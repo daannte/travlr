95a57d236b128964726dfa2b07c918a4775efc67
Dante <dkirsman@gmail.com>
2023-06-21 08:49:32 -0600

    fix: Delete dnd feature for now, need to find a new library
    The library started causing random errors on build that I can't handle,
    will find a better maintained library

--- src/pages/planner/Planner.tsx
+++ src/pages/planner/Planner.tsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { db, ref, set, remove } from "../../backend/firebase";
 import { PlannerContext, UserIdContext } from "../../App";
-import { DragDropContext, DropResult } from "@hello-pangea/dnd";
 import "./Planner.css";
 
 import Card from "../../components/card/Card";
@@ -18,7 +17,7 @@ interface PlannerProps {
 }
 
 function Planner({ savedDests, setSavedDests }: PlannerProps) {
-  const { currentPlanner, setCurrentPlanner } = useContext(PlannerContext);
+  const { currentPlanner } = useContext(PlannerContext);
   const userId = useContext(UserIdContext);
   const isSaved = savedDests.includes(currentPlanner.destination);
   const [destinationImage, setDestinationImage] = useState<string>("");
@@ -79,58 +78,6 @@ function Planner({ savedDests, setSavedDests }: PlannerProps) {
     }
   }
 
-  function handleDragDrop(results: DropResult) {
-    const { source, destination, type } = results;
-    // If we dont drag a draggrable into a droppable, we return
-    if (!destination) return;
-    // If draggable got picked and dropped in the same spot
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    ) {
-      return;
-    }
-
-    if (type === "group") {
-      // Create a copy of currentPlanner
-      const reorderedPlanner = { ...currentPlanner };
-
-      // Get the activity we want to move
-      const sourceActivitiesList =
-        reorderedPlanner.activityLists[parseInt(source.droppableId)];
-      const movedActivity = sourceActivitiesList.activities[source.index];
-
-      // Remove that activity from the array
-      sourceActivitiesList.activities.splice(source.index, 1);
-
-      // Check if it was the last element in the source list
-      const isSourceEmpty = sourceActivitiesList.activities.length === 0;
-      sourceActivitiesList.isEmpty = isSourceEmpty;
-
-      // Get the activities we want to move it to
-      const destActivitiesList =
-        reorderedPlanner.activityLists[parseInt(destination.droppableId)];
-
-      // Check if the activities list exists
-      if (destActivitiesList.isEmpty) {
-        // Create a new list with that activity if it is empty
-        destActivitiesList.activities = [movedActivity];
-
-        // Set the empty value to false since it isn't empty anymore
-        destActivitiesList.isEmpty = false;
-      } else {
-        // Insert the movedActivity into the acitivities we wanted
-        // if the activities exist
-        destActivitiesList.activities.splice(
-          destination.index,
-          0,
-          movedActivity
-        );
-      }
-      // Update the currentPlanner state with the reordered planner
-      return setCurrentPlanner(reorderedPlanner);
-    }
-  }
   return (
     <div className="planner-container">
       <div className="planner-event-container">
@@ -177,9 +124,7 @@ function Planner({ savedDests, setSavedDests }: PlannerProps) {
             />
           </div>
         </div>
-        <DragDropContext onDragEnd={handleDragDrop}>
-          <div className="planner-cards-container">{renderCards()}</div>
-        </DragDropContext>
+        <div className="planner-cards-container">{renderCards()}</div>
       </div>
       <div className="planner-map-container">Map Will go here later.</div>
     </div>
