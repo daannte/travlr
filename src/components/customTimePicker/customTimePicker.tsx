73af1a93a20bc703826aeb93d6eae62120a7ebde
Dante <dkirsman@gmail.com>
2023-06-22 09:47:50 -0600

    feat: Finished implementing the time picker
    Works great, just need to cleanup the code

--- src/components/customTimePicker/customTimePicker.tsx
+++ src/components/customTimePicker/customTimePicker.tsx
@@ -21,6 +21,7 @@ function CustomTimePicker({
   setIsChoosingTime,
 }: Props) {
   const { setCurrentPlanner } = useContext(PlannerContext);
+  const [isChoosingStartTime, setIsChoosingStartTime] = useState<boolean>(true);
   const [errorMessage, setErrorMessage] = useState("");
 
   // Set the start of the day to 12:00 AM
@@ -40,38 +41,42 @@ function CustomTimePicker({
     return timeIntervals;
   };
 
-  const handleTimeClick = (selectedTimeInterval: string) => {
-    if (!activity.startTime) {
-      setCurrentPlanner((prevPlanner) => {
-        const updatedActivityLists = prevPlanner.activityLists.map(
-          (activityList) => {
-            if (activityList.date === date) {
-              const updatedActivities = activityList.activities.map(
-                (activity, index) => {
-                  if (index === activityIndex) {
-                    return { ...activity, startTime: selectedTimeInterval };
-                  }
-                  return activity;
+  const handleStartTimeClick = (selectedTimeInterval: string) => {
+    setIsChoosingStartTime(false);
+    setCurrentPlanner((prevPlanner) => {
+      const updatedActivityLists = prevPlanner.activityLists.map(
+        (activityList) => {
+          if (activityList.date === date) {
+            const updatedActivities = activityList.activities.map(
+              (activity, index) => {
+                if (index === activityIndex) {
+                  return {
+                    ...activity,
+                    startTime: selectedTimeInterval,
+                  };
                 }
-              );
-              return { ...activityList, activities: updatedActivities };
-            }
-            return activityList;
+                return activity;
+              }
+            );
+            return { ...activityList, activities: updatedActivities };
           }
-        );
-        return { ...prevPlanner, activityLists: updatedActivityLists };
-      });
-    } else if (
+          return activityList;
+        }
+      );
+      return { ...prevPlanner, activityLists: updatedActivityLists };
+    });
+  };
+
+  const handleEndTimeClick = (selectedTimeInterval: string) => {
+    if (
       dayjs(selectedTimeInterval, "h:mm A").isBefore(
-        dayjs(activity.startTime, "h:mm A")
-      ) ||
-      dayjs(selectedTimeInterval, "h:mm A").isSame(
         dayjs(activity.startTime, "h:mm A")
       )
     ) {
-      setErrorMessage("End time must be set to a time after start time!");
-    } else if (activity.startTime && !activity.endTime) {
+      setErrorMessage("End time must be set to a time after the start time!");
+    } else {
       setErrorMessage("");
+      setIsChoosingStartTime(true);
       setCurrentPlanner((prevPlanner) => {
         const updatedActivityLists = prevPlanner.activityLists.map(
           (activityList) => {
@@ -79,7 +84,10 @@ function CustomTimePicker({
               const updatedActivities = activityList.activities.map(
                 (activity, index) => {
                   if (index === activityIndex) {
-                    return { ...activity, endTime: selectedTimeInterval };
+                    return {
+                      ...activity,
+                      endTime: selectedTimeInterval,
+                    };
                   }
                   return activity;
                 }
@@ -120,30 +128,63 @@ function CustomTimePicker({
     <div className="time-picker">
       <div className="time-picker__header">
         <input
-          className="time-picker__header-start-time"
+          className={`time-picker__header-start-time ${
+            isChoosingStartTime
+              ? "time-picker__header-start-time--outline"
+              : null
+          }`}
           placeholder="Start Time"
           value={activity.startTime}
+          onClick={() => setIsChoosingStartTime(true)}
           readOnly
         />
         <input
-          className="time-picker__header-end-time"
+          className={`time-picker__header-end-time ${
+            !isChoosingStartTime
+              ? "time-picker__header-end-time--outline"
+              : null
+          }`}
           placeholder="End Time"
           value={activity.endTime}
+          onClick={() => setIsChoosingStartTime(false)}
           readOnly
         />
       </div>
       <div className="time-picker__error">{errorMessage && errorMessage}</div>
-      <div className="time-picker__times">
-        {setTimeIntervals().map((timeInterval, index) => (
-          <div
-            className="time-picker__time"
-            key={index}
-            onClick={() => handleTimeClick(timeInterval)}
-          >
-            {timeInterval}
-          </div>
-        ))}
-      </div>
+      {isChoosingStartTime ? (
+        <div className="time-picker__times">
+          {setTimeIntervals().map((timeInterval, index) => (
+            <div
+              className={`time-picker__time ${
+                timeInterval === activity.startTime
+                  ? "time-picker__time--selected"
+                  : null
+              }`}
+              key={index}
+              onClick={() => handleStartTimeClick(timeInterval)}
+            >
+              {timeInterval}
+            </div>
+          ))}
+        </div>
+      ) : null}
+      {!isChoosingStartTime ? (
+        <div className="time-picker__times">
+          {setTimeIntervals().map((timeInterval, index) => (
+            <div
+              className={`time-picker__time ${
+                timeInterval === activity.endTime
+                  ? "time-picker__time--selected"
+                  : null
+              }`}
+              key={index}
+              onClick={() => handleEndTimeClick(timeInterval)}
+            >
+              {timeInterval}
+            </div>
+          ))}
+        </div>
+      ) : null}
       <div className="time-picker__buttons">
         <button
           className="time-picker__buttons-clear"
